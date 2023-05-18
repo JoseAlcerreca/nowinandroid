defe649c42929e0ab5945d4136785881843f319b
Jose Alcérreca <jalc@google.com>
2023-05-18 15:53:01 +0000

    Adds all foryou screenshots
    Change-Id: Id214d05c8905158da68f86f81f1b1f9a6a654451

--- feature/foryou/src/test/java/com/google/samples/apps/nowinandroid/feature/foryou/ForYouScreenScreenshotTests.kt
+++ feature/foryou/src/test/java/com/google/samples/apps/nowinandroid/feature/foryou/ForYouScreenScreenshotTests.kt
@@ -17,18 +17,31 @@
 package com.google.samples.apps.nowinandroid.feature.foryou
 
 import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.BoxWithConstraints
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.test.junit4.AndroidComposeTestRule
 import androidx.compose.ui.test.junit4.createAndroidComposeRule
+import androidx.compose.ui.test.junit4.createEmptyComposeRule
 import androidx.compose.ui.test.onRoot
-import androidx.compose.ui.unit.Dp
+import androidx.compose.ui.tooling.preview.PreviewParameter
 import androidx.compose.ui.unit.DpSize
 import androidx.compose.ui.unit.dp
+import androidx.test.core.app.ActivityScenario
+import androidx.test.ext.junit.rules.ActivityScenarioRule
 import com.github.takahirom.roborazzi.captureRoboImage
 import com.google.accompanist.testharness.TestHarness
 import com.google.samples.apps.nowinandroid.core.designsystem.theme.NiaTheme
+import com.google.samples.apps.nowinandroid.core.model.data.UserNewsResource
+import com.google.samples.apps.nowinandroid.core.ui.DevicePreviews
+import com.google.samples.apps.nowinandroid.core.ui.NewsFeedUiState
 import com.google.samples.apps.nowinandroid.core.ui.NewsFeedUiState.Success
 import com.google.samples.apps.nowinandroid.core.ui.UserNewsResourcePreviewParameterProvider
+import com.google.samples.apps.nowinandroid.feature.foryou.OnboardingUiState.Loading
 import com.google.samples.apps.nowinandroid.feature.foryou.OnboardingUiState.NotShown
 import dagger.hilt.android.testing.HiltTestApplication
+import org.junit.After
+import org.junit.Before
 import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
@@ -47,29 +60,138 @@ class ForYouScreenScreenshotTests {
      * Use a test activity to set the content on.
      */
     @get:Rule
-    val composeTestRule = createAndroidComposeRule<ComponentActivity>()
+    val composeTestRule = createEmptyComposeRule()
+
+    private lateinit var scenario: ActivityScenario<ComponentActivity>
+
+    private val userNewsResources = UserNewsResourcePreviewParameterProvider().values.first()
+
+    @Before
+    fun setup() {
+        scenario = ActivityScenario.launch(ComponentActivity::class.java)
+    }
+
+    @After
+    fun tearDown() {
+        scenario.close()
+    }
 
     @Test
-    fun testForYouScreen() {
-        composeTestRule.setContent {
-            TestHarness(size = DpSize(1000.dp, 1000.dp)) {
-                NiaTheme {
-                    ForYouScreen(
-                        isSyncing = false,
-                        onboardingUiState = NotShown,
-                        feedState = Success(
-                            feed = UserNewsResourcePreviewParameterProvider().values.first(),
-                        ),
-                        onTopicCheckedChanged = { _, _ -> },
-                        saveFollowedTopics = {},
-                        onNewsResourcesCheckedChanged = { _, _ -> },
-                        onNewsResourceViewed = {},
-                        onTopicClick = {},
-                    )
+    fun testForYouScreenPopulatedFeed() {
+        captureMultiDevice("screenshots/ForYouScreenPopulatedFeed") {
+            NiaTheme {
+                ForYouScreen(
+                    isSyncing = false,
+                    onboardingUiState = NotShown,
+                    feedState = Success(
+                        feed = userNewsResources,
+                    ),
+                    onTopicCheckedChanged = { _, _ -> },
+                    saveFollowedTopics = {},
+                    onNewsResourcesCheckedChanged = { _, _ -> },
+                    onNewsResourceViewed = {},
+                    onTopicClick = {},
+                )
+            }
+        }
+    }
+
+    @Test
+    fun testForYouScreenLoading() {
+        captureMultiDevice("screenshots/ForYouScreenLoading") {
+            NiaTheme {
+                ForYouScreen(
+                    isSyncing = false,
+                    onboardingUiState = Loading,
+                    feedState = NewsFeedUiState.Loading,
+                    onTopicCheckedChanged = { _, _ -> },
+                    saveFollowedTopics = {},
+                    onNewsResourcesCheckedChanged = { _, _ -> },
+                    onNewsResourceViewed = {},
+                    onTopicClick = {},
+                )
+            }
+        }
+    }
+
+    @Test
+    fun testForYouScreenTopicSelection() {
+
+        captureMultiDevice("screenshots/ForYouScreenTopicSelection") {
+            NiaTheme {
+                ForYouScreen(
+                    isSyncing = false,
+                    onboardingUiState = OnboardingUiState.Shown(
+                        topics = userNewsResources.flatMap { news -> news.followableTopics }
+                            .distinctBy { it.topic.id },
+                    ),
+                    feedState = NewsFeedUiState.Success(
+                        feed = userNewsResources,
+                    ),
+                    onTopicCheckedChanged = { _, _ -> },
+                    saveFollowedTopics = {},
+                    onNewsResourcesCheckedChanged = { _, _ -> },
+                    onNewsResourceViewed = {},
+                    onTopicClick = {},
+                )
+            }
+        }
+    }
+
+    @Test
+    fun testForYouScreenPopulatedAndLoading() {
+        captureMultiDevice("screenshots/ForYouScreenPopulatedAndLoading") {
+            NiaTheme {
+                ForYouScreen(
+                    isSyncing = true,
+                    onboardingUiState = OnboardingUiState.Loading,
+                    feedState = NewsFeedUiState.Success(
+                        feed = userNewsResources,
+                    ),
+                    onTopicCheckedChanged = { _, _ -> },
+                    saveFollowedTopics = {},
+                    onNewsResourcesCheckedChanged = { _, _ -> },
+                    onNewsResourceViewed = {},
+                    onTopicClick = {},
+                )
+            }
+        }
+    }
+
+
+    private fun captureMultiDevice(screenshotName: String, body: @Composable () -> Unit) {
+        listOf(
+            "phone" to "spec:shape=Normal,width=640,height=360,unit=dp,dpi=480",
+            "foldable" to "spec:shape=Normal,width=673,height=841,unit=dp,dpi=480",
+            "tablet" to "spec:shape=Normal,width=1280,height=800,unit=dp,dpi=480"
+        ).forEach {
+            captureForDevice(it.first, it.second, screenshotName, body)
+        }
+    }
+
+    private fun captureForDevice(
+        deviceName: String,
+        deviceSpec: String,
+        screenshotName: String,
+        body: @Composable () -> Unit
+    ) {
+        val (width, height) = extractSpecs(deviceSpec)
+
+        scenario.onActivity { activity ->
+            activity.setContent {
+                TestHarness(size = DpSize(width.dp, height.dp)) {
+                    body()
                 }
             }
         }
-        composeTestRule.onRoot()
-            .captureRoboImage("screenshots/ForYouScreen.png")
+        composeTestRule.onRoot().captureRoboImage("${screenshotName}_$deviceName.png")
+    }
+
+    private fun extractSpecs(deviceSpec: String): List<Int> {
+        val specs = deviceSpec.substringAfter("spec:")
+            .split(",").map { it.split("=") }.associate { it[0] to it[1] }
+        val width = specs["width"]?.toInt() ?: 640
+        val height = specs["height"]?.toInt() ?: 480
+        return listOf(width, height)
     }
 }
